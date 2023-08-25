0289cc453d2e4212f8f485930dc73e7c7fd3559c
BrunoCortez64 <125039967+BrunoCortez64@users.noreply.github.com>
2023-08-25 10:04:04 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,27 @@
+
+import CardFilm from "@/components/CardFilm";
 import Titulo from "../components/titulo";
 
 export default function Home() {
+    //mock
+    const filmes = [
+
+      {
+        titulo: "Star Wars",
+        nota: 9.5,
+        poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/dw7X9YPjjAfIxKHW04V64Bb9TB0.jpg"
+  
+      },
+      {
+        titulo: "Barbie",
+        nota: 9.2,
+        poster: "https://www.themoviedb.org/t/p/w300_and_h450_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
+  
+      }
+    ] 
+    
+    
+
   return ( //JSX
   <>
     <nav className="bg-slate-500 p-2 flex gap-3 items-end">
@@ -13,8 +34,15 @@ export default function Home() {
       </ul>
     </nav>
 
-      <Titulo texto="em alta" />
-      <Titulo texto="lançamentos" />
+      <Titulo texto="Em alta" />
+
+      <section className="flex flex-wrap gap-2">
+        {filmes.map( filme => <CardFilm filme={filme} />)}
+
+      </section>
+
+
+      <Titulo texto="Lançamentos" />
 
   </>
   )
